3652136320478cbdc32a5d2748f240c105c0945a
Abraham Ontiveros <61855947+KenkaRaidon@users.noreply.github.com>
2022-06-29 18:30:59 -0700

    index a films html

--- src/main/resources/static/assets/js/index.js
+++ src/main/resources/static/assets/js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 	$('#tabla').DataTable({
 		language: {
@@ -25,80 +25,80 @@ $(document).ready(function() {
 		},
 
 	});
-	
-	
+
+
 });
 
-function categoria(){
+function categoria() {
 	let valor = $("#selectCategoria").val()
-	if(valor=="-1"){
+	if (valor == "-1") {
 		$("#errorCategoria").html("Selecciona una opcion valida");
 	}
-	else{
+	else {
 		document.getElementById("formCategoria").submit();
 	}
 }
 
-function actor(){
+function actor() {
 	$("#tituloModalCambiar").html("Filtrar por actor");
-	 $('#formCambiar').attr('action', '/filtroActor');
-	
+	$('#formCambiar').attr('action', '/filtroActor');
+
 }
 
-function titulo(){
+function titulo() {
 	$("#tituloModalCambiar").html("Filtrar por titulo");
-	 $('#formCambiar').attr('action', '/filtroTitulo');
-	
+	$('#formCambiar').attr('action', '/filtroTitulo');
+
 }
 
-function detalles(filmId){
-	$.get("http://localhost:8080/detallesFilm/" + filmId, function(data, status){
+function detalles(filmId) {
+	$.get("http://localhost:8666/detallesFilm/" + filmId, function (data, status) {
 		console.log(data);
 		console.log(status);
-		
-		let detalles=data.detalles;
-		let categorias=data.categorias;
-		let actores=data.actores;
-		
-		let categorias1="";
-		
+
+		let detalles = data.detalles;
+		let categorias = data.categorias;
+		let actores = data.actores;
+
+		let categorias1 = "";
+
 		$("#detalleNumPelicula").html(detalles.filmId);
 		$("#detalleTitulo").html(detalles.title);
-		$("#detallePrecio").html("$"+detalles.rentalRate);
+		$("#detallePrecio").html("$" + detalles.rentalRate);
 		$("#detalleLanzamiento").html(detalles.releaseYear);
 		$("#detalleDuracion").html(detalles.length + " min");
-		
-		categorias.forEach(c => categorias1 += c+", ");
-		categorias1=categorias1.slice(0,categorias1.length-2)
+
+		categorias.forEach(c => categorias1 += c + ", ");
+		categorias1 = categorias1.slice(0, categorias1.length - 2)
 		$("#detalleCategorias").html(categorias1);
 		$("#detalleLenguaje").html(detalles.language.name.trim());
 		$("#detalleClasificacion").html(detalles.rating);
 		$("#detalleDescripcion").html(detalles.description);
-		
+
 		let lista = document.getElementById("detalleActores");
-		lista.innerHTML="";
-		
-		for(let i=0; i<actores.length; i++){
+		lista.innerHTML = "";
+
+		for (let i = 0; i < actores.length; i++) {
 			let li = document.createElement("li");
-			li.innerHTML=actores[i];
+			li.innerHTML = actores[i];
 			lista.appendChild(li);
 		}
-		
+
 		$("#modalDetalles").modal("show");
-		
-	 });
+
+	});
 }
 
-$("#botonExportarPDF").on("click", function() {
-		let filmId = $("#detalleNumPelicula").html();
-		
-		var win = window.open($('meta[name=urlBase]').attr("content") + "detallesFilm/exportarpdf/" + filmId, '_blank');
-		if (win) {
-			//Browser has allowed it to be opened
-			win.focus();
-		} else {
-			//Browser has blocked it
-			alert('Please allow popups for this website');
-		}
-		
-	});
+$("#botonExportarPDF").on("click", function () {
+	let filmId = $("#detalleNumPelicula").html();
+
+	var win = window.open($('meta[name=urlBase]').attr("content") + "detallesFilm/exportarpdf/" + filmId, '_blank');
+	if (win) {
+		//Browser has allowed it to be opened
+		win.focus();
+	} else {
+		//Browser has blocked it
+		alert('Please allow popups for this website');
+	}
+
+});
